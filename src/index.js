c56d3698a1a60dbdb62d38829493232e6b04dacb
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-02-13 01:20:29 +0200

    Add main and app components

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-const bodyElement = document.querySelector(`body`);
-bodyElement.textContent = `Hello, hello, hello!`;
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app.jsx";
+
+
+const Settings = {
+  PLACES_COUNT: 123
+};
+
+
+ReactDOM.render(
+    <App
+      placesCount = {Settings.PLACES_COUNT}
+    />,
+    document.querySelector(`#root`)
+);
